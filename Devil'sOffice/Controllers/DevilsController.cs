0e5f95ee06fcb48df49921946e3b6f4ef8127c05
LetY0uDown <maks.manin.04@gmail.com>
2024-10-17 14:47:15 +1000

    Добавьте файлы проекта.

---
+++ Devil'sOffice/Controllers/DevilsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Devil_sOffice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevilsController : ControllerBase
+    {
+        readonly _666Context _context;
+        public DevilsController(_666Context _context)
+        {
+            this._context = _context;
+        }
+
+        [HttpPost("AddDevil")]
+        public async Task<ActionResult> AddDevil(Devil devil)
+        {
+            _context.Devils.Add(devil);
+            await _context.SaveChangesAsync();
+            return Ok("Дьявол создан!");
+        }
+
+        [HttpPost("UpdateDevil")]
+        public async Task<ActionResult> UpdateDevil(Devil devil)
+        {
+            _context.Devils.Update(devil);
+            await _context.SaveChangesAsync();
+            return Ok("Дьявол обновился!");
+        }
+
+        [HttpPost("DeleteDevil")]
+        public async Task<ActionResult> DeleteDevil(Devil devil)
+        {
+            Disposal disposal = (Disposal)_context.Disposals.Where(c => c.Title == devil.Nick && c.Year == devil.Year);
+            _context.Disposals.Add(disposal);
+            _context.Devils.Remove(devil);
+            await _context.SaveChangesAsync();
+            return Ok("Дьявол стёрся!");
+        }
+    }
+}
---
+++ Devil'sOffice/Controllers/RacksController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Devil_sOffice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RacksController : ControllerBase
+    {
+        readonly _666Context _context;
+        public RacksController(_666Context _context)
+        {
+            this._context = _context;
+        }
+
+        [HttpPost("AddRack")]
+        public async Task<ActionResult> AddRack(Rack rack)
+        {
+            _context.Racks.Add(rack);
+            await _context.SaveChangesAsync();
+            return Ok("Стеллаж создан!");
+        }
+
+        [HttpPost("UpdateRack")]
+        public async Task<ActionResult> UpdateRack(Rack rack)
+        {
+            _context.Racks.Update(rack);
+            await _context.SaveChangesAsync();
+            return Ok("Стеллаж обновился!");
+        }
+
+        [HttpPost("DeleteRack")]
+        public async Task<ActionResult> DeleteRack(Rack rack)
+        {
+            Disposal disposal = (Disposal)_context.Disposals.Where(c => c.Title == rack.Title && c.Year == rack.YearBuy);
+            _context.Disposals.Add(disposal);
+            _context.Racks.Remove(rack);
+            await _context.SaveChangesAsync();
+            return Ok("Стеллаж умер!");
+        }
+    }
+}
